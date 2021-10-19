7bb6bc55d9a3b65c375133a1daf9f25a5f9bbaca
Sadekujjaman Saju[recruit] <eng@recruit.so>
2021-10-19 21:22:18 +0600

    Add update.js

---
+++ update.js
@@ -0,0 +1,26 @@
+const NavBar: FC = () => {
+    const [pageSwtichButtons, setPageSwitchButtons] = useState(
+      InitialPageSwitchButtons
+    );
+dfsdfsd
+  
+    return (
+        <div className="d-flex flex-row align-items-center navbar-desktop-right-content">
+          {pageSwtichButtons.map((button) => {
+                      className="navbar-page-switch-button-icon"
+                      kjkjnkjn
+                    />
+                    kjnkjn
+                    {button.name}
+                  </div>
+                </Link>
+              </div>
+            );
+          })}
+          <NavbarAccountDropdown />
+          {/* {this.navbarAccountDropdown()} */}
+        </div>
+      </div>
+    );
+  };
+  
